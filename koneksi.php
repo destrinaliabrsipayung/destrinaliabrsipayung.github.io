636d91afd1e39fe552dd497a7ee5b4d4ca5d80bd
Destri Sipayung <destrisipayung17@gmail.com>
2026-04-15 13:08:08 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,12 @@
+<?php
+$host = "sql303.infinityfree.com"; // MySQL Hostname dari InfinityFree
+$user = "if0_41069941";        // MySQL Username dari InfinityFree
+$pass = "321DNSyes";    // Password akun InfinityFree
+$db   = "if0_410669941_seed_library";     // Nama database yang kamu buat
 
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+if (!$koneksi) {
+    die("Gagal terhubung ke database: " . mysqli_connect_error());
+}
+?>
